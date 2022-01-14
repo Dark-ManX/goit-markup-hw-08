3bbd5e8db005f10e5641648cc88e74ce6bccc5c6
Dark-ManX <sergiosis@ukr.net>
2022-01-14 03:18:33 +0200

    14.01 3:18

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -2,13 +2,13 @@
     const menuBtnRef = document.querySelector("[data-menu-button]");
     const mobileMenuRef = document.querySelector("[data-menu]");
     
-    menuBtRef.addEventsListener("click", () => {
+    menuBtnRef.addEventListener("click", () => {
         const expanded =
             menuBtnRef.getAttribute("aria-expanded") === "true" || false;
 
-        menuBtnRef.classList.togle("is-open");
+        menuBtnRef.classList.toggle("menu-button--is-open");
         menuBtnRef.setAttribute("artia-expanded", !expanded);
 
-        mobileMenuRef.classList.togle("is-open");
+        mobileMenuRef.classList.togle("menu-button--is-open");
     });
 })();
